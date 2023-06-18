e7bceea3f3bf81fdd2e472a2bc69e04c74009514
bettayub <bettayub19@gmail.com>
2023-06-18 14:19:21 +0300

    completed js functions

--- speedDetector.js
+++ speedDetector.js
@@ -1,5 +1,16 @@
-// speed detector section
 function calculateSpeed() {
-    let max =70;
-    let mySpeed = document.getElementById("input_test")
+    let max = 70;
+    let mySpeed = parseInt(document.getElementById("input_test").value);
+    let points = (mySpeed - max) / 5;
+    let result;
+
+    if (points < 1) {
+        result = 'OK';
+    } else if (points >= 12) {
+        result = "License Suspended";
+    } else {
+        result = "Points:Invalid Entry" + points ;
+    }
+
+    document.getElementById("result").innerText = result;
 }
