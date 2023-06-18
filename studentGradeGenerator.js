8c72d0361ab79fbbcb365fb105b45412d8b6bd3b
bettayub <bettayub19@gmail.com>
2023-06-18 21:28:13 +0300

    declared variables and assigned values to variables

--- studentGradeGenerator.js
+++ studentGradeGenerator.js
@@ -1,34 +1,20 @@
-// Student Grade Detector
-function mymarks() {
-    let marks = parseInt(prompt("Enter your marks between 0 and 100 "));
-    let destination = document.getElementById("grade_output");
-    
-    if (marks<0 || marks>100) {
-        destination.innerHTML = "Out of Range Entry."
-    }
-    else if (marks>79) {
-        destination.innerHTML = "Excellent!You scored an A."
-    }
+let marks = 70;
+let result;
 
-    else if (marks>=60 && marks<=79) {
-        destination.innerHTML = "Good!You scored a B."
-    }
-
-    else if (marks>=49 && marks<=59) {
-        destination.innerHTML = "You scored a C."
-    }
-    
-    else if (marks>=40 && marks<=49) {
-        destination.innerHTML = "You scored a D."
-    }
-
-    else if (marks<40) {
-        destination.innerHTML = "You scored a E."
-    }
-
-    else {
-        destination.innerHTML = "Invalid input!"
-    }
+if (marks < 0 || marks > 100) {
+    result = 'Out of Range Entry.';
+} else if (marks > 79) {
+    result = 'Excellent! You scored an A.';
+} else if (marks >= 60 && marks <= 79) {
+    result = 'Good! You scored a B.';
+} else if (marks >= 49 && marks <= 59) {
+    result = 'You scored a C.';
+} else if (marks >= 40 && marks <= 49) {
+    result = 'You scored a D.';
+} else if (marks < 40) {
+    result = 'You scored an E.';
+} else {
+    result = 'Invalid input!';
 }
 
-
+console.log(result);
