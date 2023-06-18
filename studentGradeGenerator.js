3dbefa507c83e59e36eb233b0cde18e0a534f8ad
bettayub <bettayub19@gmail.com>
2023-06-18 21:06:41 +0300

    declared variables and assigned values to variables

---
+++ studentGradeGenerator.js
@@ -0,0 +1,14 @@
+let mySpeed = 133;
+let max = 70;
+let points = (mySpeed - max) / 5;
+let result;
+
+if (points < 1) {
+    result = 'OK';
+} else if (points >= 12) {
+    result = 'License Suspended';
+} else {
+    result = 'Points: ' + points;
+}
+
+console.log(result);
